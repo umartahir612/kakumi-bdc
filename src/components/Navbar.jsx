4cf0da6e894df41dcccb7352c9dfbd31d43bb2cd
Abubakar Umar Tahir <umartaheerabubakar@gmail.com>
2025-08-24 04:20:25 -0700

    Updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,69 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
+  // Close navbar when a link is clicked
+  const closeNavbar = () => {
+    const navbar = document.getElementById("navbarNav");
+    if (navbar.classList.contains("show")) {
+      navbar.classList.remove("show");
+    }
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: 'var(--brand-blue)'}}>
+    <nav
+      className="navbar navbar-expand-lg navbar-dark"
+      style={{ backgroundColor: "var(--brand-blue)" }}
+    >
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-        <img src="/logo.png" alt="Kakumi Logo" style={{ height: "40px", marginRight: "10px" }}/>
-           KAKUMI BDC LTD
+        <Link className="navbar-brand d-flex align-items-center" to="/">
+          <img
+            src="/logo192.png" // <-- change to your logo path
+            alt="Kakumi BDC"
+            style={{ height: "30px", marginRight: "10px" }}
+          />
+          KAKUMI BDC LTD
         </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
-            <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/services">Services</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/rates">Rates</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/compliance">Compliance</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/" onClick={closeNavbar}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about" onClick={closeNavbar}>
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/services" onClick={closeNavbar}>
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/rates" onClick={closeNavbar}>
+                Rates
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/compliance" onClick={closeNavbar}>
+                Compliance
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact" onClick={closeNavbar}>
+                Contact
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
